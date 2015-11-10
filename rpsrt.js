78bc306a61e2ea2bc4ca1d65c7cd1556790bd8a0
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-11-10 02:43:13 -0600

    finishing up skeleton user register form

--- rpsrt.js
+++ rpsrt.js
@@ -78,6 +78,173 @@ if (Meteor.isClient) {
       }
     },
 
+    "change #mf_avatar": function (event) {
+      var dropSelect = document.getElementById("mf_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_mf_avatar").label = "Choose a sex from list below";
+      } else if (choice1 === "male") {
+        console.log("Male choice");
+        document.getElementById('male_avatar').style.display = '';
+        document.getElementById('mf_avatar').style.display = 'none';    
+      } else {
+        console.log("Female choice");
+        document.getElementById('female_avatar').style.display = '';
+        document.getElementById('mf_avatar').style.display = 'none';   
+      } 
+    },
+
+    "change #cat_avatar": function (event) {
+      var dropSelect = document.getElementById("cat_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_cat_avatar").label = "Choose an avatar from list below";
+      } else if (choice1 === "jedi") {
+        console.log("Cat Jedi choice");
+      } else if (choice1 === "warrior") {
+        console.log("Cat Warrior choice");
+      } else if (choice1 === "wizard") {
+        console.log("Cat Wizard choice");
+      } else {
+        console.log("Cat Healer choice");
+      } 
+    },
+
+    "change #dog_avatar": function (event) {
+      var dropSelect = document.getElementById("dog_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_dog_avatar").label = "Choose an avatar from list below";
+      } else if (choice1 === "jedi") {
+        console.log("Dog Jedi choice");
+      } else if (choice1 === "warrior") {
+        console.log("Dog Warrior choice");
+      } else if (choice1 === "wizard") {
+        console.log("Dog Wizard choice");
+      } else {
+        console.log("Dog Healer choice");
+      } 
+    },
+
+    "change #male_avatar": function (event) {
+      var dropSelect = document.getElementById("male_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_male_avatar").label = "Choose an avatar from list below";
+      } else if (choice1 === "jedi") {
+        console.log("Male Jedi choice");
+      } else if (choice1 === "warrior") {
+        console.log("Male Warrior choice");
+      } else if (choice1 === "wizard") {
+        console.log("Male Wizard choice");
+      } else {
+        console.log("Male Healer choice");
+      } 
+    },
+
+    "change #female_avatar": function (event) {
+      var dropSelect = document.getElementById("female_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_female_avatar").label = "Choose an avatar from list below";
+      } else if (choice1 === "jedi") {
+        console.log("Female Jedi choice");
+      } else if (choice1 === "warrior") {
+        console.log("Female Warrior choice");
+      } else if (choice1 === "wizard") {
+        console.log("Female Wizard choice");
+      } else {
+        console.log("Female Healer choice");
+      } 
+    },
+
+    "change #abstract_avatar": function (event) {
+      var dropSelect = document.getElementById("abstract_avatar");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_abstract_avatar").label = "Choose an avatar from list below";
+      } else if (choice1 === "pat1") {
+        console.log("Pattern 1 choice");
+      } else if (choice1 === "pat2") {
+        console.log("Pattern 2 choice");
+      } else if (choice1 === "pat3") {
+        console.log("Pattern 3 choice");
+      } else {
+        console.log("Pattern 4 choice");
+      } 
+    },
+
+    "change #rps_images": function (event) {
+      var dropSelect = document.getElementById("rps_images");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_rps_images").label = "Choose an RPS image set from list below";
+      } else if (choice1 === "cat") {
+        console.log("Cat RPS images");
+        document.getElementById('cat_rps_images').style.display = '';
+        document.getElementById('rps_images').style.display = 'none';
+      } else if (choice1 === "dog") {
+        console.log("Dog RPS images");
+        document.getElementById('dog_rps_images').style.display = '';
+        document.getElementById('rps_images').style.display = 'none';
+      } else if (choice1 === "human") {
+        console.log("Human choices");
+        document.getElementById('mf_rps_images').style.display = '';
+        document.getElementById('rps_images').style.display = 'none';
+      } else if (choice1 === "objects") {
+        console.log("Actual object choices");
+        document.getElementById('objects_choices').style.display = '';
+        document.getElementById('rps_images').style.display = 'none';
+      } else {
+        console.log("Custom RPS images");
+        document.getElementById('custom_rock').style.display = '';
+        document.getElementById('custom_paper').style.display = '';
+        document.getElementById('custom_scissors').style.display = '';
+        document.getElementById('rps_images').style.display = 'none';
+      }
+    },
+
+    "change #objects_choices": function (event) {
+      var dropSelect = document.getElementById("objects_choices");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_objects_choices").label = "Choose object RPS images from list below";
+      } else if (choice1 === "pat1") {
+        console.log("Pattern 1 choice");
+        document.getElementById('object1_rps_images').style.display = '';
+        document.getElementById('objects_choices').style.display = 'none';
+      } else {
+        console.log("Pattern 2 choice");
+        document.getElementById('object2_rps_images').style.display = '';
+        document.getElementById('objects_choices').style.display = 'none';
+      } 
+    },
+
+    "change #mf_rps_images": function (event) {
+      var dropSelect = document.getElementById("mf_rps_images");
+      var choice1 = dropSelect.value;
+      console.log(choice1);
+      if (choice1 === "") {
+        document.getElementById("choose_mf_rps_images").label = "Choose a sex from list below";
+      } else if (choice1 === "male") {
+        console.log("Male choice");
+        document.getElementById('male_rps_images').style.display = '';
+        document.getElementById('mf_rps_images').style.display = 'none';    
+      } else {
+        console.log("Female choice");
+        document.getElementById('female_rps_images').style.display = '';
+        document.getElementById('mf_rps_images').style.display = 'none';   
+      } 
+    },
+
     "submit .new-user": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
